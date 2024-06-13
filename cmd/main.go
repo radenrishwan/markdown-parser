618c80b029c975617c805b3f68b4b622584aae8e
radenrishwan <radenrishwan@gmail.com>
2024-06-13 20:40:12 +0700

    add close token, and fix when store is not empty on scan

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
-import "log"
+import (
+	"errors"
+	"log"
+)
 
 func main() {
-	text := "#Hello, World\n  ##Hello, World\n*An italic text*"
+	text := "#Hello, World\n  ##Hello, World\n*An italic text*\n**An bold text**"
 
 	scan := NewScanner(text, keyword)
 
@@ -20,39 +23,71 @@ const (
 	H4 = "HEADING 4"
 	H5 = "HEADING 5"
 
-	BOLD           = "BOLD"
-	ITALIC         = "ITALIC"
-	BLOCKQUOTE     = "BLOCKQUOTE"
-	UNORDERED_LIST = "UNORDERED_LIST"
+	CH1 = "CLOSE HEADING 1"
+	CH2 = "CLOSE HEADING 2"
+	CH3 = "CLOSE HEADING 3"
+	CH4 = "CLOSE HEADING 4"
+	CH5 = "CLOSE HEADING 5"
+
+	BOLD    = "BOLD"
+	CBOLD   = "CLOSE BOLD"
+	ITALIC  = "ITALIC"
+	CITALIC = "CLOSE ITALIC"
+
+	BLOCKQUOTE  = "BLOCKQUOTE"
+	CBLOCKQUOTE = "CLOSE BLOCKQUOTE"
+
+	UNORDERED_LIST = "UNORDERED_LIST" // TODO: implement later
 	ORDERED_LIST   = "ORDERED_LIST"
 
-	CODE            = "CODE"
+	CODE  = "CODE"
+	CCODE = "CLOSE CODE"
+
 	HORIZONTAL_RULE = "HORIZONTAL_RULE"
 	// LINK            = "LINK" // TODO: implement later
 	// IMAGE           = "IMAGE"
 
-	EOL  = "END OF LINE"
-	TEXT = "TEXT"
-	NL   = "NEWLINE"
+	EOL   = "END OF LINE"
+	TEXT  = "TEXT"
+	CTEXT = "CLOSE TEXT"
+	NL    = "NEWLINE"
 
 	// TODO: add extended syntax later
 )
 
 var keyword = map[string]string{
-	"#":     "<h1>",
-	"##":    "<h2>",
-	"###":   "<h3>",
-	"####":  "<h4>",
-	"#####": "<h5>",
+	H1: "<h1>",
+	H2: "<h2>",
+	H3: "<h3>",
+	H4: "<h4>",
+	H5: "<h5>",
 
-	"**":           "<b>",
-	"*":            "<i>",
-	">":            "<blockquote>",
-	"-":            "<ul>",
-	"ORDERED_LIST": ORDERED_LIST, // TODO: gonna try something later
-	"`":            "<code>",
-	"---":          "<hr>",
-	"\n":           "<br>",
+	CH1: "</h1>",
+	CH2: "</h2>",
+	CH3: "</h3>",
+	CH4: "</h4>",
+	CH5: "</h5>",
+
+	BOLD:  "<b>",
+	CBOLD: "</b>",
+
+	ITALIC:  "<i>",
+	CITALIC: "</i>",
+
+	BLOCKQUOTE:  "<blockquote>",
+	CBLOCKQUOTE: "</blockquote>",
+
+	UNORDERED_LIST: "<ul>",
+	ORDERED_LIST:   "<ol>", // TODO: gonna try something later
+
+	CODE:  "<code>",
+	CCODE: "</code>",
+
+	HORIZONTAL_RULE: "<hr>",
+	NL:              "<br>",
+
+	TEXT:  "<p>",
+	CTEXT: "</p>",
 }
 
 type Token struct {
@@ -91,15 +126,30 @@ func (scanner *Scanner) Scan() []Token {
 
 		current := scanner.advance()
 		log.Println("Current char: ", string(current))
+		log.Printf("current: %d \n", scanner.Current)
+
+		if scanner.text == false && store != "" {
+			temp := scanner.Tokens[len(scanner.Tokens)-1]
+
+			scanner.Tokens = append(scanner.Tokens[0:len(scanner.Tokens)-1], Token{
+				Type:  TEXT,
+				Line:  scanner.Line,
+				Value: store,
+			}, temp)
+
+			store = ""
+		}
+
 		switch current {
 		case '\n':
 			scanner.Tokens = append(scanner.Tokens, Token{
 				Type:  NL,
 				Line:  scanner.Line,
-				Value: "\n",
+				Value: scanner.Keyword[NL],
 			})
 
 			scanner.Line += 1
+			scanner.text = false
 		case ' ':
 			if scanner.text == true {
 				store += string(current) // store value if not a TOKENS
@@ -111,7 +161,7 @@ func (scanner *Scanner) Scan() []Token {
 				scanner.Tokens = append(scanner.Tokens, Token{
 					Type:  H5,
 					Line:  scanner.Line,
-					Value: scanner.Keyword["######"],
+					Value: scanner.Keyword[H5],
 				})
 
 				scanner.Current += 4
@@ -119,7 +169,7 @@ func (scanner *Scanner) Scan() []Token {
 				scanner.Tokens = append(scanner.Tokens, Token{
 					Type:  H4,
 					Line:  scanner.Line,
-					Value: scanner.Keyword["####"],
+					Value: scanner.Keyword[H4],
 				})
 
 				scanner.Current += 3
@@ -127,7 +177,7 @@ func (scanner *Scanner) Scan() []Token {
 				scanner.Tokens = append(scanner.Tokens, Token{
 					Type:  H3,
 					Line:  scanner.Line,
-					Value: scanner.Keyword["###"],
+					Value: scanner.Keyword[H3],
 				})
 
 				scanner.Current += 2
@@ -135,7 +185,7 @@ func (scanner *Scanner) Scan() []Token {
 				scanner.Tokens = append(scanner.Tokens, Token{
 					Type:  H2,
 					Line:  scanner.Line,
-					Value: scanner.Keyword["##"],
+					Value: scanner.Keyword[H2],
 				})
 
 				scanner.Current += 1
@@ -143,7 +193,7 @@ func (scanner *Scanner) Scan() []Token {
 				scanner.Tokens = append(scanner.Tokens, Token{
 					Type:  H1,
 					Line:  scanner.Line,
-					Value: scanner.Keyword["#"],
+					Value: scanner.Keyword[H1],
 				})
 			}
 
@@ -151,22 +201,42 @@ func (scanner *Scanner) Scan() []Token {
 		case '*':
 			// check next character * again or not
 			if scanner.peek(1) == "*" {
-				scanner.Tokens = append(scanner.Tokens, Token{
-					Type:  BOLD,
-					Line:  scanner.Line,
-					Value: scanner.Keyword["**"],
-				})
+				// check if close or open
+				c, _ := scanner.checkPrevToken(len(scanner.Tokens) - 1)
+				if c.Type == BOLD {
+					scanner.Tokens = append(scanner.Tokens, Token{
+						Type:  CBOLD,
+						Line:  scanner.Line,
+						Value: scanner.Keyword[CBOLD],
+					})
+				} else {
+					scanner.Tokens = append(scanner.Tokens, Token{
+						Type:  BOLD,
+						Line:  scanner.Line,
+						Value: scanner.Keyword[BOLD],
+					})
+				}
 
 				scanner.Current += 1
 				scanner.text = false
 				continue
 			}
 
-			scanner.Tokens = append(scanner.Tokens, Token{
-				Type:  ITALIC,
-				Line:  scanner.Line,
-				Value: scanner.Keyword["*"],
-			})
+			c, _ := scanner.checkPrevToken(len(scanner.Tokens) - 1)
+			if c.Type == ITALIC {
+				scanner.Tokens = append(scanner.Tokens, Token{
+					Type:  CITALIC,
+					Line:  scanner.Line,
+					Value: scanner.Keyword[CITALIC],
+				})
+			} else {
+				scanner.Tokens = append(scanner.Tokens, Token{
+					Type:  ITALIC,
+					Line:  scanner.Line,
+					Value: scanner.Keyword[ITALIC],
+				})
+			}
+
 			scanner.text = false
 		case '-':
 			// check next character -- or not
@@ -174,7 +244,7 @@ func (scanner *Scanner) Scan() []Token {
 				scanner.Tokens = append(scanner.Tokens, Token{
 					Type:  HORIZONTAL_RULE,
 					Line:  scanner.Line,
-					Value: scanner.Keyword["-"],
+					Value: scanner.Keyword[HORIZONTAL_RULE],
 				})
 
 				scanner.Current += 2
@@ -185,7 +255,7 @@ func (scanner *Scanner) Scan() []Token {
 			scanner.Tokens = append(scanner.Tokens, Token{
 				Type:  ORDERED_LIST,
 				Line:  scanner.Line,
-				Value: scanner.Keyword["-"],
+				Value: scanner.Keyword[ORDERED_LIST],
 			})
 
 			scanner.text = false
@@ -193,7 +263,7 @@ func (scanner *Scanner) Scan() []Token {
 			scanner.Tokens = append(scanner.Tokens, Token{
 				Type:  CODE,
 				Line:  scanner.Line,
-				Value: scanner.Keyword["`"],
+				Value: scanner.Keyword[CODE],
 			})
 
 			scanner.text = false
@@ -202,21 +272,22 @@ func (scanner *Scanner) Scan() []Token {
 			scanner.text = true
 		}
 
-		if scanner.text == false && store != "" {
-			scanner.Tokens = append(scanner.Tokens, Token{
-				Type:  TEXT,
-				Line:  scanner.Line,
-				Value: store,
-			})
-
-			store = ""
-		}
-
-		log.Printf("current: %d - len: %d", scanner.Current, len(scanner.Source))
-
 		if scanner.Current-1 == len(scanner.Source) {
 			break
 		}
+	}
+
+	// need to check in case store is not empty
+	if scanner.text == false && store != "" {
+		temp := scanner.Tokens[len(scanner.Tokens)-1]
+
+		scanner.Tokens = append(scanner.Tokens[0:len(scanner.Tokens)-1], Token{
+			Type:  TEXT,
+			Line:  scanner.Line,
+			Value: store,
+		}, temp)
+
+		store = ""
 	}
 
 	return scanner.Tokens
@@ -236,9 +307,18 @@ func (scanner *Scanner) advance() byte {
 
 func (scanner *Scanner) peek(lot int) string {
 	// check if source is shorter than lot
+	log.Printf("check peek: %d - len: %d", scanner.Current+lot, len(scanner.Source))
 	if scanner.Current+lot > len(scanner.Source) {
 		return ""
 	}
 
-	return string(scanner.Source[scanner.Current+lot])
+	return string(scanner.Source[scanner.Current : scanner.Current+lot])
+}
+
+func (scanner *Scanner) checkPrevToken(index int) (Token, error) {
+	if len(scanner.Tokens) < index {
+		return Token{}, errors.New("out of index")
+	}
+
+	return scanner.Tokens[index], nil
 }
